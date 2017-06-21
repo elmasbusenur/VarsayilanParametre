76da6bed8f730d1c9f0469e43902b32b86958208
elmasbusenur <elmasbusenur@gmail.com>
2017-06-21 15:22:52 +0300

    parametre kullanımı

---
+++ VarsayilanParametre.py
@@ -0,0 +1,6 @@
+def kayıt_ekle(ad="bilgi yok",soyad="bilgi yok",yas="bilgi yok",meslek="bilgi yok"):
+    print("kayıt ekleniyor...")
+    print("ad:{}\nsoyad:{}\nyas:{}\nmeslek:{}".format(ad,soyad,yas,meslek))
+
+
+kayıt_ekle(ad="busenur",meslek="bilg.müh")
